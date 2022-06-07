7c96b495ffc0f3a4cbb9954f186667f1de699ab1
Solomon Olufelo <93885102+solufelo@users.noreply.github.com>
2022-06-07 00:51:44 -0400

    Changed helmet logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
   return (
     
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-2 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[70px] flex justify-between items-center px-3 bg-[#0a192f] text-gray-300'>
       <div>
       <li> 
         <Link to="home" smooth={true} duration={500} >
-        <img src={Logo} alt='Logo' style={{ width: '70px' }} />
+        <img src={Logo} alt='Logo' style={{ width: '80px' }} />
         </Link>
                             </li>
       </div>
