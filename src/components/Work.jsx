bb2560ead6f1794a7bd2b2f47a75c082bc40dc5f
Solomon Olufelo <93885102+solufelo@users.noreply.github.com>
2022-06-10 20:11:51 -0400

    Removed unfilled work, and added closed casket cover

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import WorkImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
+
+import closedCasket from '../assets/closedcasket.png'
 const Work = () => {
   return (
     <div name="work" className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
@@ -15,7 +15,7 @@ const Work = () => {
                 
                 {/* Grid Item */}
 
-                <div style={{backgroundImage: `url(${WorkImg})`}}
+                <div style={{backgroundImage: `url(${closedCasket})`}}
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
 
                     {/* Hover Effects */}
@@ -34,104 +34,10 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
-
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Play</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>    
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
-
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Play</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>    
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
-
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Play</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>    
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
-
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Play</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>    
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
-
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Play</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>    
-                            </a>
-                        </div>
+                
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
   )
 }
 
